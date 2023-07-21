8b49aa2a71f7f3dbc01d1361cef9097ce65a252f
Jeyma Rodriguez <jeymarp97@gmail.com>
2023-07-21 18:06:43 -0400

    NoteActivity updated

--- app/src/main/java/com/zybooks/finalproject/NoteActivity.java
+++ app/src/main/java/com/zybooks/finalproject/NoteActivity.java
@@ -1,32 +1,50 @@
 package com.zybooks.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.zybooks.finalproject.repo.MemoRepository;
+import android.os.Bundle;
 
-import android.app.Application;
-
-import com.zybooks.finalproject.model.memoRepository;
-
-import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
 
-   // private final memoRepository mMemoRepository;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
 
-   /* public NoteActivity(Application application) {
-        super(application);
-        mMemoRepository = memoRepository.getInstance(application.getApplicationContext());
+        // Initialize MemoRepository
+        MemoRepository memoRepository = MemoRepository.getInstance(this);
+
+        // Display the NoteDetailFragment
+        openNoteDetailFragment();
+
+        // Handle the "Add Memo" floating action button click
+        findViewById(R.id.add_memo_button).setOnClickListener(v -> {
+            openNoteDetailFragment(); // Pass -1 to indicate that it's a new note
+        });
     }
 
-    public LiveData<List<memoRepository>> getSubjects() {
-        return mMemoRepository.getMemos();
+    // Method to open the NoteDetailFragment for displaying/editing a specific note
+    private void openNoteDetailFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Create the NoteDetailFragment and pass the noteId as an argument
+        NoteDetailFragment noteDetailFragment = new NoteDetailFragment();
+        Bundle args = new Bundle();
+        args.putLong("noteId", -1);
+        noteDetailFragment.setArguments(args);
+
+        // Replace the fragment_container with the NoteDetailFragment
+        fragmentTransaction.replace(R.id.fragment_container, noteDetailFragment);
+
+        // Add the transaction to the back stack(optional)
+        fragmentTransaction.addToBackStack(null);
+
+        // Commit the transaction
+        fragmentTransaction.commit();
     }
 
-    public void addMemo(memoRepository memo) {
-        mMemoRepository.addMemo(memo);
-    }
-
-    public void deleteMemo(memoRepository memo) {
-        memoRepository.deleteMemo(memo);
-    }*/
 }
