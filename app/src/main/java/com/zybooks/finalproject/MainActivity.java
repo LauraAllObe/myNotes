6520070320c3cfda7bc834c8d159248df2d36866
Jeyma Rodriguez <jeymarp97@gmail.com>
2023-08-02 01:11:52 -0400

    fragment note detail landscape layout

--- app/src/main/java/com/zybooks/finalproject/MainActivity.java
+++ app/src/main/java/com/zybooks/finalproject/MainActivity.java
@@ -8,22 +8,9 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.Html;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-
-import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String PREFS_NAME = "MyPrefs";
-    private static final String DARK_THEME_PREF = "darkThemePref";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,64 +25,12 @@ public class MainActivity extends AppCompatActivity {
                     Builder(navController.getGraph()).build();
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
         }
-
-        // Read the user's theme preference from SharedPreferences
-        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        boolean isDarkTheme = preferences.getBoolean(DARK_THEME_PREF, false);
-
-        // Set the theme based on the user's preference
-        if (isDarkTheme) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        }
-
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return navController.navigateUp() || super.onSupportNavigateUp();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.appbar_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_dark_theme) {
-            // Toggle the dark theme preference
-            toggleDarkTheme();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void toggleDarkTheme() {
-        // Read the user's theme preference from SharedPreferences
-        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        boolean isDarkTheme = preferences.getBoolean(DARK_THEME_PREF, false);
-
-        // Save the new theme preference (toggle the value)
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean(DARK_THEME_PREF, !isDarkTheme);
-        editor.apply();
-
-        // Apply the selected theme
-        if (!isDarkTheme) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        }
-
-        // Recreate the activity to apply the theme changes
-        recreate();
     }
 
 }
